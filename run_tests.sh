5ab7bb2a58d46491c130454bebf15eb0e7da3f76
Zac Brannelly <zac.brannelly@gmail.com>
2026-05-24 21:49:21 +1000

    Fail run_tests.sh script if "All tests passed" is not found

--- run_tests.sh
+++ run_tests.sh
@@ -1,7 +1,14 @@
 #!/bin/bash
 
+set -e
+
 export ZOS_RUN_TESTS=ON
 
 ./scripts/fetch_qemu_efi.sh
 ./build.sh
-./scripts/run_qemu.sh
+
+./scripts/run_qemu.sh | awk '
+    { print; fflush() }
+    /All tests passed/ { found=1; exit 0 }
+    END { if (!found) exit 1 }
+'
