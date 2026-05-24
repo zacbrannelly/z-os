a204ab5b370c7bd1808c33c689b1716dc773f2a5
Zac Brannelly <zac.brannelly@gmail.com>
2026-05-24 22:43:02 +1000

    Attempt to run QEMU headless in CI

--- scripts/run_qemu.sh
+++ scripts/run_qemu.sh
@@ -2,17 +2,27 @@
 
 ESP_DIR="$(pwd)/esp"
 
-qemu-system-aarch64 \
-  -machine virt \
-  -cpu cortex-a72 \
-  -m 1024 \
-  -bios ./build/QEMU_EFI.fd \
-  -drive if=virtio,format=raw,file=fat:rw:${ESP_DIR} \
-  -serial stdio \
-  -monitor none \
-  -device ramfb \
-  -device virtio-gpu-pci \
-  -device qemu-xhci \
-  -usb \
-  -device usb-kbd \
+qemu_args=(
+  -machine virt
+  -cpu cortex-a72
+  -m 1024
+  -bios ./build/QEMU_EFI.fd
+  -drive if=virtio,format=raw,file=fat:rw:${ESP_DIR}
+  -serial stdio
+  -monitor none
+)
+
+qemu_args+=(
+  -device ramfb
+  -device virtio-gpu-pci
+  -device qemu-xhci
+  -usb
+  -device usb-kbd
   -device usb-mouse
+)
+
+if [ "${ZOS_RUN_TESTS:-OFF}" = "ON" ]; then
+  qemu_args+=(-display none)
+fi
+
+qemu-system-aarch64 "${qemu_args[@]}"
